03738840be930cf3e455fb54c0e42a0689925644
Michael Chrupcala <michael@artblocks.io>
2025-01-30 22:56:29 -0500

    UI rough draft finished

--- src/api/tasks.tsx
+++ src/api/tasks.tsx
@@ -1,15 +1,16 @@
-export const getTask = async () => {
-  const url = process.env.NEXT_APP_API_URL;
-  console.log(url);
+import { tasksResponse } from "../../sample-tasks-response";
+import { TaskResponse } from "@/app/page";
+export const getTask = (): TaskResponse | null => {
+  //TODO: CORS is blocked locally -- will this work if deployed to Vercel? Do I want to or...nah? How do I present this?
+  // const url = process.env.NEXT_PUBLIC_API_URL || "";
+  // console.log(url);
 
   try {
     // const res = await fetch(url);
-    // console.log(res);
-    return {
-      taskID: "123456789",
-      STATUS: "SUCCESS",
-    };
+    console.log(tasksResponse);
+    return tasksResponse;
   } catch (error) {
     console.warn("Something went wrong: ", error);
+    return null;
   }
 };
